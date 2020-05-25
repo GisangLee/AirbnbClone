10a84a7f1c8411fec76a445bef9c49b57b298f8d
GisangLee <kisang6710@gmail.com>
2020-05-25 21:48:11 +0900

    Title : pagination and detail view
    Content : make pagination for page and
              detail view of room

--- rooms/views.py
+++ rooms/views.py
@@ -1,24 +1,22 @@
-from math import ceil
+from django.views.generic import ListView
+from django.http import Http404
 from django.shortcuts import render
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    off_set = limit - page_size
-    all_rooms = models.Room.objects.all()[off_set:limit]
-    page_count = models.Room.objects.count() / page_size
-    return render(
-        request,
-        "rooms/all_rooms.html",
-        context={
-            "rooms": all_rooms,
-            "page":page,
-            "page_count": ceil(page_count),
-            "page_range": range(1, ceil(page_count)),
-        }
-    )
+class HomeView(ListView):
+    """ Home View Definition """
 
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+    context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", context={"room": room})
+    
+    except models.Room.DoesNotExist:
+        raise Http404()
