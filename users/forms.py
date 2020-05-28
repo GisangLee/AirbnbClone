ac64bb17e4314c6f7dd8dcd77ccad158fbfc2d58
GisangLee <masterkorea01@naver.com>
2020-05-28 14:34:17 +0900

    Title : Log In and Sign Up Design
    Content : Finishing Log in & Sign up Design with Tailwind CSS

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,7 @@ from . import models
 
 class LoginForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Password"}))
 
     def clean(self):
         email = self.cleaned_data.get("email")
@@ -22,13 +22,28 @@ class LoginForm(forms.Form):
 
 
 class SignUpForm(forms.ModelForm):
-
     class Meta:
         model = models.User
         fields = ("first_name", "last_name", "email")
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Email"}),
+        }
 
     password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "password"}))
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}),
+                                label="Confirm Password")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError(
+                "That Email is already taken", code="existing_user"
+            )
+        except models.User.DoesNotExist:
+            return email
 
     def clean_password1(self):
         password = self.cleaned_data.get("password")
